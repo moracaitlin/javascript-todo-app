48aa08a857da7b7ffb0aa8fb52fda17a4071c444
moracaitlin <moracaitlin@gmail.com>
2015-12-14 20:32:49 -0500

    add delete button that deletes individual todos

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,7 +26,7 @@ function renderTodos() {
   for (var i = 0; i < todos.length; i++) {
     var todo = todos[i];
     var checked = todo.complete ? 'checked' : ''
-    $('.todos').append("<li class='todo " + checked + "' data-id=" + todo.id + "><label><input class='toggle-todo' type='checkbox' " + checked + "/> " + todo.text + "</label></li>");
+    $('.todos').append("<li class='todo " + checked + "' data-id=" + todo.id + "><label><input class='toggle-todo' type='checkbox' " + checked + "/> " + todo.text + "</label><button>delete</button></li>");
   }
 
   var total = todos.length;
@@ -92,6 +92,24 @@ $(document).ready(function() {
 
     $('.todo-text').val('');
     $('.alert').text('');
+
+  });
+
+  //Have delete button delete single todo.
+  $(document).on('click', 'li button', function(event) {
+    var id = $(event.target).parent().data('id');
+    var todo = findById(id);
+    var indexOfTodo = todos.indexOf(todo);
+    console.log(indexOfTodo);
+
+    if (indexOfTodo > -1) {
+      todos.splice(indexOfTodo, 1);
+    }
+
+  $(document).on("click", "li button", function(e) {
+    e.preventDefault();
+    $(this).parent().remove();
+  });
 
   });
 
